bebf7367ae97d209574b776aa07bc62a671f7d91
Pedro Samuel <rodriguezcaudillopedrosamuel@gmail.com>
2025-11-19 14:31:50 -0600

    feat(auth): agregar grant type password a cliente Swagger

--- src/cima.Domain/OpenIddict/OpenIddictDataSeedContributor.cs
+++ src/cima.Domain/OpenIddict/OpenIddictDataSeedContributor.cs
@@ -127,7 +127,10 @@ public class OpenIddictDataSeedContributor : IDataSeedContributor, ITransientDep
                 consentType: OpenIddictConstants.ConsentTypes.Implicit,
                 displayName: "Swagger Application",
                 secret: null,
-                grantTypes: new List<string> { OpenIddictConstants.GrantTypes.AuthorizationCode, },
+                grantTypes: new List<string> { 
+                    OpenIddictConstants.GrantTypes.AuthorizationCode,
+                    OpenIddictConstants.GrantTypes.Password
+                },
                 scopes: commonScopes,
                 redirectUris: new List<string> { $"{swaggerRootUrl}/swagger/oauth2-redirect.html" },
                 clientUri: swaggerRootUrl.EnsureEndsWith('/') + "swagger",
