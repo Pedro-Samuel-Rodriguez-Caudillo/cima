a4c56975b866c900c6a3aed4c317f02a1e00a015
Pedro Samuel <rodriguezcaudillopedrosamuel@gmail.com>
2025-11-24 19:55:09 -0600

    feat(http): proxy configurado

---
+++ src/cima.HttpApi.Client/ClientProxies/cima/ContactRequests/ContactRequestClientProxy.cs
@@ -0,0 +1,7 @@
+// This file is part of ContactRequestClientProxy, you can customize it here
+// ReSharper disable once CheckNamespace
+namespace cima.ContactRequests;
+
+public partial class ContactRequestClientProxy
+{
+}
---
+++ src/cima.HttpApi.Client/ClientProxies/cima/Listings/ListingClientProxy.cs
@@ -0,0 +1,7 @@
+// This file is part of ListingClientProxy, you can customize it here
+// ReSharper disable once CheckNamespace
+namespace cima.Listings;
+
+public partial class ListingClientProxy
+{
+}
