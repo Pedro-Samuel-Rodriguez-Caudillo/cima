5f2ac1dc85a76709279cf8f970cffd7dd587b6a1
Pedro Samuel <rodriguezcaudillopedrosamuel@gmail.com>
2025-12-04 14:32:47 -0600

    chore(db): agregar migracion para FirstPublishedAt en Listing

---
+++ src/cima.EntityFrameworkCore/Migrations/20251204184816_AddFirstPublishedAtToListing.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cima.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFirstPublishedAtToListing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FirstPublishedAt",
+                table: "Listings",
+                type: "timestamp without time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstPublishedAt",
+                table: "Listings");
+        }
+    }
+}
