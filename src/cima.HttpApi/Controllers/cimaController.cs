018a1931e94e83795a08e1c789ad6134706a1537
Pedro Samuel <rodriguezcaudillopedrosamuel@gmail.com>
2025-12-15 18:55:56 -0600

    chore: http consultas

--- src/cima.HttpApi/Controllers/cimaController.cs
+++ src/cima.HttpApi/Controllers/cimaController.cs
@@ -1,10 +1,12 @@
-﻿using cima.Localization;
+using cima.Localization;
+using Microsoft.AspNetCore.Mvc;
 using Volo.Abp.AspNetCore.Mvc;
 
 namespace cima.Controllers;
 
 /* Inherit your controllers from this class.
  */
+[IgnoreAntiforgeryToken]
 public abstract class cimaController : AbpControllerBase
 {
     protected cimaController()
